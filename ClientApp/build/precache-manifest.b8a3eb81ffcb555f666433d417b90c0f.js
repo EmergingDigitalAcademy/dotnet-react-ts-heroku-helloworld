9d7daaf2668f7fed77373fb22ab3a3e579cf2c89
Blaine Booher <blaine@emergingprairie.com>
2020-07-10 16:08:42 -0500

    adding build/ folder to git repo for testing

---
+++ ClientApp/build/precache-manifest.b8a3eb81ffcb555f666433d417b90c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad648b01bb2a7528e3db45d0f7bc112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa91a84466074229288",
+    "url": "/static/css/2.d6500151.chunk.css"
+  },
+  {
+    "revision": "fe20064836a376744819",
+    "url": "/static/css/main.6636e503.chunk.css"
+  },
+  {
+    "revision": "faa91a84466074229288",
+    "url": "/static/js/2.6b3011ea.chunk.js"
+  },
+  {
+    "revision": "f0c1093415b06c9dc10783dbb242bbcc",
+    "url": "/static/js/2.6b3011ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe20064836a376744819",
+    "url": "/static/js/main.7efe9cb4.chunk.js"
+  },
+  {
+    "revision": "7fb120a2e835c18c81fc",
+    "url": "/static/js/runtime-main.97fcea3e.js"
+  }
+]);
